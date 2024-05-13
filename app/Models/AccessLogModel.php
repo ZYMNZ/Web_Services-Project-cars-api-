864be3cb18ae27cf382f42884ec500c64cf38eea
ARSH5588 <arsh013@hotmail.com>
2024-05-12 20:31:23 -0400

    Changed Expiry date and commented the logged timestamp

--- app/Models/AccessLogModel.php
+++ app/Models/AccessLogModel.php
@@ -32,7 +32,7 @@ class AccessLogModel extends BaseModel
         $log_data["user_id"] = $account_data["id"];
         $log_data["email"] = $account_data["email"];
         $log_data["user_action"] = $user_action;
-        $log_data["logged_at"] = DateTimeHelper::getDateAndTime(DateTimeHelper::M_D_Y_H_M_S);
+//        $log_data["logged_at"] = DateTimeHelper::getDateAndTime(DateTimeHelper::M_D_Y_H_M_S);
         return $this->insert($this->table_name, $log_data);
     }
 }
