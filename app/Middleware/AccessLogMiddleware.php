f937ddfebbecb57c923d39e0ac74726791bc725c
Yaman Alhamamy <yamanmh2002@gmail.com>
2024-05-16 22:32:12 -0400

    Update: AccessLogMiddleware (testing)

--- app/Middleware/AccessLogMiddleware.php
+++ app/Middleware/AccessLogMiddleware.php
@@ -2,9 +2,23 @@
 
 namespace Vanier\Api\Middleware;
 
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Vanier\Api\Helpers\LoggerHelper;
 
 class AccessLogMiddleware extends LoggerHelper
 {
-
+    public function process(Request $request, RequestHandler $handler): ResponseInterface
+    {
+        $access_log = LoggerHelper::accessLogger();
+        $access_log->info(sprintf('%s %s',$request->getMethod(), $request->getUri()));
+        $response = $handler->handle($request);
+        $access_log->info(sprintf(
+            '%d %s',
+            $response->getStatusCode(),
+            $response->getReasonPhrase(),
+        ));
+        return $response;
+    }
 }
