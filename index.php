1238412b0bde81e3f7ce7915079011ceb68b9fa7
Yaman Alhamamy <yamanmh2002@gmail.com>
2024-05-20 23:44:10 -0400

    Add:Versioning

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ $errorMiddleware->getDefaultErrorHandler()->forceContentType(APP_MEDIA_TYPE_JSON
 
 //TODO: change the name of the subdirectory here.
 // You also need to change it in .htaccess
-$app->setBasePath("/cars-api");
+$app->setBasePath("/cars-api/v2");
 
 // Include the file that contains the application routes. 
 //* NOTE: your routes must be managed in the api_routes.php file.
